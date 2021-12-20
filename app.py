7f52621f5fd5dede3e05e701545aaa00b9d4d0a1
brkatia <brkatia12@gmail.com>
2021-12-21 00:11:49 +0100

    homepage

--- app.py
+++ app.py
@@ -5,8 +5,5 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-<<<<<<< HEAD
-    return "hi "
-=======
-    return "new line"
->>>>>>> 74ceb66ed163901afcdb835d5a357bbc9a91be31
+
+    return "home page"
